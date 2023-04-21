d792c6df12e504d43f4c99a56d3854afb0ecd310
dhanashree <dhanashreephuge18@gmail.com>
2023-04-21 21:01:18 +0530

    On branch routedemo

--- src/index.js
+++ src/index.js
@@ -69,8 +69,38 @@ import 'bootstrap/dist/css/bootstrap.css';
 // root.render(<ShowList  />);
 
 //// FillComboBox
-import FillComboBox from './fill-combo-box';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FillComboBox  />);
+// import FillComboBox from './fill-combo-box';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<FillComboBox  />);
+
+////////////////// create static site for show route demo
+//BrowserRouter, Routes, Route 
+/// npm i -D react-router-dom  (nnot find module 'react-router-dom')
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
 
 reportWebVitals();
